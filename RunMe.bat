c5a8917bf2566fe7af1f197f99860c46c8615369
Zack <izaac.corbett@gmail.com>
2015-06-22 16:54:28 -0500

    Added recimg command for recovery partition

--- RunMe.bat
+++ RunMe.bat
@@ -1,3 +1,8 @@
 diskpart /s %~dp0diskpart.txt
 Dism /apply-image /imagefile:%CD%\fake.wim /index:1 /ApplyDir:C:\
 C:\Windows\System32\bcdboot C:\Windows /l en-US
+xcopy %~dp0fake.wim R:\CustomRefresh.wim
+cd C:\Windows\System32
+C:
+recimg /setcurrent R:\
+recimg /setcurrent D:\
